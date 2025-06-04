f27de9f705dc8c558ddb2aa947d386475d1f2991
nariman <nariman.wk@gmail.com>
2025-06-04 06:04:57 +0300

    Тестовое задание

--- app/components/QrCodeGenerator.php
+++ app/components/QrCodeGenerator.php
@@ -6,6 +6,12 @@ use Endroid\QrCode\Builder\Builder;
 
 class QrCodeGenerator
 {
+    /**
+     * Генерирует QR код из ссылки
+     *
+     * @param string $url
+     * @return string
+     */
     public function generate(string $url):string
     {
         $qrCode = Builder::create()
